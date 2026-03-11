db5a67a6db6125058bd9cb04596c1c8275256e40
jonnycs <jonnydevvs@gmail.com>
2026-03-10 20:24:07 -0700

    Display seek buttons on YouTube video player

---
+++ contentScript.js
@@ -0,0 +1,24 @@
+(() => {
+  let youtubeLeftControls, youtubePlayer;
+
+  const addSeekBtns = () => {
+    const rewindBtn = document.createElement("img");
+    const fastForwardBtn = document.createElement("img");
+
+    rewindBtn.src = chrome.runtime.getURL("assets/rewind.svg");
+    rewindBtn.className = "ytp-button " + "rewind-btn";
+    rewindBtn.title = "Click to rewind 10 seconds";
+
+    fastForwardBtn.src = chrome.runtime.getURL("assets/fast-forward.svg");
+    fastForwardBtn.className = "ytp-button " + "fast-forward-btn";
+    fastForwardBtn.title = "Click to fast forward 10 seconds";
+
+    youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+    youtubePlayer = document.getElementsByClassName("video-stream")[0];
+
+    youtubeLeftControls.appendChild(rewindBtn);
+    youtubeLeftControls.appendChild(fastForwardBtn);
+  }
+
+  addSeekBtns();
+})();
