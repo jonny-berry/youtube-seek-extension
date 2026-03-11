73fa38085a44962416b488bc377d92412f12ceb6
jonnycs <jonnydevvs@gmail.com>
2026-03-10 22:47:04 -0700

    Update seek buttons to use saved jump amount

--- contentScript.js
+++ contentScript.js
@@ -1,20 +1,19 @@
 (() => {
   let youtubeLeftControls, youtubePlayer;
 
+  const rewindBtn = document.createElement("img");
+  const fastForwardBtn = document.createElement("img");
+
   const addSeekBtns = () => {
     const seekBtnsContainer = document.createElement("div");
-    const rewindBtn = document.createElement("img");
-    const fastForwardBtn = document.createElement("img");
 
     seekBtnsContainer.className = "seek-btns-container";
-    
+
     rewindBtn.src = chrome.runtime.getURL("assets/rewind.svg");
     rewindBtn.className = "ytp-button " + "rewind-btn " + "seek-ext-btn";
-    rewindBtn.title = "Click to rewind 10 seconds";
 
     fastForwardBtn.src = chrome.runtime.getURL("assets/fast-forward.svg");
     fastForwardBtn.className = "ytp-button " + "fast-forward-btn " + "seek-ext-btn";
-    fastForwardBtn.title = "Click to fast forward 10 seconds";
 
     youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
     youtubePlayer = document.getElementsByClassName("video-stream")[0];
@@ -22,15 +21,29 @@
     youtubeLeftControls.appendChild(seekBtnsContainer);
     seekBtnsContainer.appendChild(rewindBtn);
     seekBtnsContainer.appendChild(fastForwardBtn);
+  }
 
-    rewindBtn.addEventListener("click", () => {
-      youtubePlayer.currentTime -= 10;
-    })
+  const initSeekButtons = () => {
+    chrome.storage.sync.get("seekDuration", (result) => {
+      let seekDuration = parseInt(result.seekDuration) || 10;
 
-    fastForwardBtn.addEventListener("click", () => {
-      youtubePlayer.currentTime += 10;
+      rewindBtn.title = `Click to rewind ${seekDuration} seconds`;
+
+      fastForwardBtn.title = `Click to fast forward ${seekDuration} seconds`;
+
+      rewindBtn.onclick = () => { youtubePlayer.currentTime -= seekDuration; };
+
+      fastForwardBtn.onclick = () => { youtubePlayer.currentTime += seekDuration; };
     })
   }
 
   addSeekBtns();
+  initSeekButtons();
+
+  // Update seek button functionality when seek duration changes
+  chrome.storage.onChanged.addListener((changes) => {
+    if (changes.seekDuration) {
+      initSeekButtons();
+    }
+  });
 })();
