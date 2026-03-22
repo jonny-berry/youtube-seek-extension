7eb8fa5660daf40c18ccb6bdc828106441aa0847
jonnycs <jonnydevvs@gmail.com>
2026-03-22 11:03:51 -0700

    Display tooltip on seek button hover

--- contentScript.js
+++ contentScript.js
@@ -18,6 +18,66 @@
     youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
     youtubePlayer = document.getElementsByClassName("video-stream")[0];
 
+
+
+    function addToolTip(eventTarget) {
+      const toolTipContainer = document.createElement("div");
+      toolTipContainer.className = "ytp-tooltip " + "ytp-bottom " + "seek-ext-tooltip";
+
+      const btnRect = fastForwardBtn.getBoundingClientRect();
+      const playerRect = youtubePlayer.getBoundingClientRect();
+
+      let left, top;
+
+      if (eventTarget === fastForwardBtn) {
+        left = (btnRect.left - playerRect.left + btnRect.width / 2) - (playerRect.height * 0.15);;
+        top = btnRect.top - playerRect.top - toolTipContainer.offsetHeight - (playerRect.height * 0.091);
+      }
+
+      else if (eventTarget === rewindBtn) {
+        left = (btnRect.left - playerRect.left + btnRect.width / 2) - (playerRect.height * 0.19);;
+        top = btnRect.top - playerRect.top - toolTipContainer.offsetHeight - (playerRect.height * 0.091);
+      }
+
+      toolTipContainer.style.left = `${left}px`;
+      toolTipContainer.style.top = `${top}px`;
+
+      const textWrapper = document.createElement('div');
+      textWrapper.className = "ytp-tooltip-text-wrapper";
+
+      const bottomText = document.createElement('div');
+      bottomText.className = "ytp-tooltip-bottom-text";
+
+      const text = document.createElement('span');
+      text.className = "ytp-tooltip-text";
+      text.textContent = eventTarget.title;
+
+      const videoPlayer = document.getElementsByClassName("html5-video-player")[0];
+
+      videoPlayer.appendChild(toolTipContainer);
+      toolTipContainer.appendChild(textWrapper);
+
+      textWrapper.appendChild(bottomText);
+      bottomText.appendChild(text);
+    }
+
+
+
+    function removeToolTip() {
+      const videoPlayer = document.getElementsByClassName("html5-video-player")[0];
+      const toolTipContainer = document.getElementsByClassName("seek-ext-tooltip")[0];
+
+      videoPlayer.removeChild(toolTipContainer)
+    }
+
+
+
+    fastForwardBtn.addEventListener('mouseenter', () => { addToolTip(event.currentTarget);  })
+    rewindBtn.addEventListener('mouseenter', () => { addToolTip(event.currentTarget);  })
+
+    fastForwardBtn.addEventListener('mouseleave', () => { removeToolTip();  })
+    rewindBtn.addEventListener('mouseleave', () => { removeToolTip();  })
+
     youtubeLeftControls.appendChild(seekBtnsContainer);
     seekBtnsContainer.appendChild(rewindBtn);
     seekBtnsContainer.appendChild(fastForwardBtn);
@@ -27,9 +87,9 @@
     chrome.storage.sync.get("seekDuration", (result) => {
       let seekDuration = parseInt(result.seekDuration) || 10;
 
-      rewindBtn.title = `Click to rewind ${seekDuration} seconds`;
+      rewindBtn.title = `Rewind ${seekDuration} seconds`;
 
-      fastForwardBtn.title = `Click to fast forward ${seekDuration} seconds`;
+      fastForwardBtn.title = `Fast forward ${seekDuration} seconds`;
 
       rewindBtn.onclick = () => { youtubePlayer.currentTime -= seekDuration; };
 
